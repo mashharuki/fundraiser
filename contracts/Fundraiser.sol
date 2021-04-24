144a28428b407beea524f9d3a0c2a526f5a5db81
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-24 22:52:09 +0900

    テストコードを追加しました。

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,10 +1,17 @@
 pragma solidity >0.4.23;
 
 contract Fundraiser {
+    // 変数を宣言する。
     string public name;
+    string public url;
+    string public imageURL;
+    string public description; 
 
     // コンストラクター
-    constructor (string memory _name) public {
+    constructor (string memory _name, string memory _url, string memory _imageURL, string memory _description) public {
         name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        description = _description;
     }
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -7,16 +7,34 @@ contract ("Fundraiser", accounts => {
     let fundraiser;
     // 受取人の名前
     const name = "Beneficiary Name";
+    // 詳細を確認できるURL
+    const url = "beneficiaryname.org";
+    // 画像のURL
+    const imageURL = "https://placekitten.com/600/350";
+    // 簡単な説明
+    const description = "Beneficiary description";
     
     describe ("initialization", () => {
         // テストが実行される前に資金調達を設定する。
         beforeEach (async () => {
-            fundraiser = await FundraiserContract.new(name);
+            fundraiser = await FundraiserContract.new(name, url, imageURL, description);
         });
         // 資金調達の名前とコンストラクタに渡した名前が一致していることを確認
         it ("gets the beneficiary name", async () => {
             const actual = await fundraiser.name();
             assert.equal(actual, name, "names should match");
+        });
+        it ("gets the beneficiary url", async () => {
+            const actual = await fundraiser.url();
+            assert.equal(actual, url, "url should match");
+        });
+        it ("gets the beneficiary imageURL", async () => {
+            const actual = await fundraiser.imageURL();
+            assert.equal(actual, imageURL, "imageURL should match");
+        });
+        it ("gets the beneficiary description", async () => {
+            const actual = await fundraiser.description();
+            assert.equal(actual, description, "description should match");
         });
     });
 });
