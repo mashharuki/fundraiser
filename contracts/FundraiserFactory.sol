78dbb01a798c8908f40cb61c68d3d34f2a34ae98
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-25 21:06:59 +0900

    Fundraiserインスタンスを生成するコードを追記

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -1,5 +1,25 @@
 pragma solidity >0.4.23;
 
+import "./Fundraiser.sol";
+
 contract FundraiserFactory {
-    
+    // Fundraiser型の配列
+    Fundraiser[] private _fundraisers;
+
+    /**
+     * インスタンス数を取得する関数
+     */
+    function fundraisersCount () public view returns (uint256) {
+        return _fundraisers.length;
+    }
+
+    /**
+     * インスタンス生成関数
+     */
+    function createFundraiser (string memory name, string memory url, string memory imageURL, string memory description, address payable beneficiary) public {
+        // インスタンスを生成
+        Fundraiser fundraiser = new Fundraiser (name, url, imageURL, description, beneficiary, msg.sender);
+        // 配列に格納する。
+        _fundraisers.push(fundraiser);
+    }
 }
--- test/fundraiser_factory_test.js
+++ test/fundraiser_factory_test.js
@@ -10,3 +10,32 @@ contract ("FundraiserFactory: deployment", () => {
         assert(fundraiserFactory, "fundraiser factory was not deployed") 
     });
 });
+
+// Fundraiserコントラクトインスタンス作成用テストコード
+contract ("FundraiserFactory: createfundraiser", (accounts) => {
+    let FundraiserFactory;
+    // 以下、fundraiserの引数
+    // 受取人の名前
+    const name = "Beneficiary Name";
+    // 詳細を確認できるURL
+    const url = "beneficiaryname.org";
+    // 画像のURL
+    const imageURL = "https://placekitten.com/600/350";
+    // 簡単な説明
+    const description = "Beneficiary description";
+    // 受取人のアドレス
+    const beneficiary = accounts[1];
+
+    // Fundraiserコントラクトを作成するテストコード
+    it ("increments the fundraisersCount", async () => {
+        fundraiserFactory = await FundraiserFactoryContract.deployed();
+        // インスタンスの数を取得する。
+        const currentFundraisersCount = await fundraiserFactory.fundraisersCount();
+        // インスタンスを作成する。
+        await fundraiserFactory.createFundraiser(name, url, imageURL, description, beneficiary);
+        // インスタンスの数を取得する。
+        const newFundraisersCount = await fundraiserFactory.fundraisersCount();
+        // 差異を確認する。
+        assert.equal((newFundraisersCount - currentFundraisersCount), 1, "should increment by 1");
+    });
+});
