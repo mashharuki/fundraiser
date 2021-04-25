ea9ac5218e0de3feb056face27607ce4823faaf3
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-25 16:39:21 +0900

    寄付の総額を算出するまでをコミットする。

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,8 +1,12 @@
 pragma solidity >0.4.23;
 
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
-contract Fundraiser {
+
+contract Fundraiser is Ownable {
+    // SafeMathが適用されるように設定する。
+    using SafeMath for uint256;
     // 変数を宣言する。
     string public name;
     string public url;
@@ -12,6 +16,15 @@ contract Fundraiser {
     address payable public beneficiary;
     // 管理人のアドレス
     address public custodian;
+    // 構造体
+    struct Donation {
+        uint256 value;
+        uint256 date; 
+    }
+    // 寄付者と寄付情報を保持するためのmap
+    mapping (address => Donation[]) private _donations;
+    // 寄付総額
+    uint256 public totalDonations;
 
     /**
      * コンストラクター
@@ -23,5 +36,51 @@ contract Fundraiser {
         description = _description;
         beneficiary = _beneficiary;
         transferOwnership(_custodian);
+    }
+
+    /**
+     * 受取人のアドレスを設定する関数
+     */
+    function setBeneficiary (address payable _beneficiary) public onlyOwner {
+        beneficiary = _beneficiary;
+    }
+
+    /**
+     * 寄付数をカウントする関数
+     */
+    function myDonationsCount () public view returns (uint256) {
+        return _donations[msg.sender].length;
+    }
+
+    /**
+     * 寄付を実行するdonate関数
+     */
+    function donate () public payable {
+        // 寄付の情報をセットする。
+        Donation memory donation = Donation({
+            value: msg.value,
+            date: block.timestamp
+        });
+        // マップ変数に追加する。
+        _donations[msg.sender].push(donation);
+        // 寄付総額の算出
+        totalDonations = totalDonations.add(msg.value);
+      }
+
+    /**
+     * 寄付のリストを作成する。
+     */
+    function myDonations () public view returns (uint256[] memory values, uint256[] memory dates) {
+        // 寄付数を取得する。
+        uint256 count = myDonationsCount();
+        values = new uint256[](count);
+        dates = new uint256[](count);
+        // 寄付のリストを作成する。
+        for (uint256 i = 0; i < count; i++) {
+            Donation storage donation = _donations[msg.sender][i];
+            values[i] = donation.value;
+            dates[i] = donation.date;
+        }
+        return (values, dates);
     }
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -18,11 +18,13 @@ contract ("Fundraiser", accounts => {
     // 管理人のアドレス
     const owner = accounts[0];
     
+    // テストが実行される前に資金調達を設定する。
+    beforeEach (async () => {
+        fundraiser = await FundraiserContract.new(name, url, imageURL, description, beneficiary, owner);
+    });
+
+    // 各変数の初期設定用テストコード
     describe ("initialization", () => {
-        // テストが実行される前に資金調達を設定する。
-        beforeEach (async () => {
-            fundraiser = await FundraiserContract.new(name, url, imageURL, description, beneficiary, owner);
-        });
         // 資金調達の名前とコンストラクタに渡した名前が一致していることを確認
         it ("gets the beneficiary name", async () => {
             const actual = await fundraiser.name();
@@ -49,6 +51,68 @@ contract ("Fundraiser", accounts => {
         it ("gets the owner", async () => {
             const actual = await fundraiser.owner();
             assert.equal(actual, owner, "bios should match");
+        });
+    });
+
+    // setBeneficiary関数用のテストコード
+    describe ("setBeneficiary", () => {
+        // 新しい受取人のアドレスを設定する。
+        const newBeneficiary = accounts[2];
+
+        it ("updated beneficiary when called by owner account", async () => {
+            // setBeneficiary関数の呼び出し
+            await fundraiser.setBeneficiary (newBeneficiary, {from: owner});
+            // 受取人のアドレスを設定
+            const actualBeneficiary = await fundraiser.beneficiary();
+            assert.equal(actualBeneficiary, newBeneficiary, "beneficiaries should match");
+        });
+        // 所有者以外から呼び出された時にエラーを出力するかどうかチェックする。
+        it ("throws an error when called from a non-owner account", async => {
+            try {
+                // setBeneficiary関数の呼び出し
+                await fundraiser.setBeneficiary (newBeneficiary, {from: accounts[3]});
+                assert.fail("withdraw was not restricted to owners")
+            } catch (err) {
+                const expectedError = "Ownable: caller is not the owner";
+                const actualError = err.reason;
+                assert.equal(actualError, expectedError, "should not be permitted");
+            }
+        });
+    });
+
+    // 寄付のテストコード
+    describe ("making donations", () => {
+        // 寄付額
+        const value = web3.utils.toWei('0.0289');
+        // 寄付者のアドレス
+        const donor = accounts[2];
+
+        it ("increases myDonationsCount", async () => {
+            // myDonationsCount関数の呼び出し(寄付数を増やす。)
+            const currentDonationsCount = await fundraiser.myDonationsCount({from: donor});
+            // donate関数の呼び出し
+            await fundraiser.donate({from: donor, value});
+            // myDonationsCount関数の呼び出し
+            const newDonationsCount = await fundraiser.myDonationsCount({from: donor});
+            assert.equal(1, newDonationsCount - currentDonationsCount, "myDonationsCount should increment by 1");
+        });
+
+        it ("include donation in myDonations", async () => {
+            // 寄付関数を実行する。
+            await fundraiser.donate({from: donor, value});
+            // myDonations関数を呼び出す。
+            const {values, dates} = await fundraiser.myDonations({from: donor});
+            assert.equal(value, values[0], "values should match");
+            assert (dates[0], "date should be present");
+        });
+
+        it ("increase the totalDonations amount", async () => {
+            const currentTotalDonations = await fundraiser.totalDonations();
+            await fundraiser.donate ({from: donor, value});
+            const newTotalDonations = await fundraiser.totalDonations();
+            // 差異を算出する。
+            const diff = newTotalDonations - currentTotalDonations;
+            assert.equal (diff, value, "difference should match the donation value");
         });
     });
 });
